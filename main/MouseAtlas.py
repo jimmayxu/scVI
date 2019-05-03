086e3bafd1370fd2654c0de77e1a28975cdbf0bd
Jimmy Xu <zx3@sanger.ac.uk>
2019-05-03 16:50:50 +0100

    change the _inti_ for the sake of running folder scvi

--- main/MouseAtlas.py
+++ main/MouseAtlas.py
@@ -15,7 +15,13 @@ adata = sc.read_10x_h5(os.path.join( save_path, "MouseAtlas.total.h5ad" ))
 
 
 import scvi
+
 from dataset.anndata import AnnDataset
 from inference import UnsupervisedTrainer
 from models.vae import VAE
 from typing import Tuple
+
+
+import scvi
+
+from models.modules import Encoder
--- scvi/models/__init__.py
+++ scvi/models/__init__.py
@@ -1,6 +1,6 @@
 from .classifier import Classifier
 from .scanvi import SCANVI
-from .vae import VAE, LDVAE
+#from .vae import VAE, LDVAE
 from .vae_fish import VAEF
 from .vaec import VAEC
 
